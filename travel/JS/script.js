bc377172874bc4f3e284802c86db5d1a73d4c539
Rodion <rodionserez@gmail.com>
2022-07-12 10:38:33 +0300

    feat: add adaptive section-destinations

--- travel/JS/script.js
+++ travel/JS/script.js
@@ -1,12 +1,62 @@
 const icon_menu = document.querySelector('.menu__icon');
 const navigation = document.querySelector('#navigation');
 const cross = document.querySelector('.close_window_cross');
+const circles_slider =  document.getElementById('scroll_circles');
+const destinations_container = document.getElementById('destinations_container');
+const screenWidth = window.screen.width;
 let Close_menu_flag = false;
+
+/* переменная для хранения предыдущей ширины экрана */
+var last_screenWidth;
+
+
+/* number_ON переменная для хранения номера активного круга в слайдере */
+function switch_slider(number_ON) {
+  const svg_circles = document.querySelectorAll('.svg_circle');
+  const circles = document.querySelectorAll('.scroll_circle');
+  if (circles.length > 0) {
+    for (let i = 0; i < svg_circles.length; i++) {
+      var order = window.getComputedStyle(svg_circles[i]).order;
+      order = Number(order);
+      if (order === number_ON - 1) { circles[i].style="fill: #F2785C" }
+      else { circles[i].style="fill: rgba(242, 120, 92, 0.5)" }
+    }
+  }
+}
+
+function window_resize() {
+  const screenWidth = window.screen.width;
+  if(last_screenWidth <= 390 && screenWidth > 390) {
+    destinations_container.after(circles_slider);
+    switch_slider(2);
+  }
+  else if (screenWidth <= 390 && last_screenWidth > 390) {
+    destinations_container.append(circles_slider);
+    switch_slider(1);
+    const scroll_circles = document.querySelector('#scroll_circles');
+    scroll_circles.style="bottom: 12px";
+  }
+  last_screenWidth = screenWidth;
+}
 
 function Open_close_menu() {
   document.body.classList.toggle('_lock');
   navigation.classList.toggle('_active');
 }
+
+
+window_resize();
+if (screenWidth > 390) switch_slider(2);
+else {
+  switch_slider(1);
+  scroll_circles.style="bottom: -32px";
+}
+
+
+window.addEventListener('resize', function(e){
+  window_resize();
+});
+
 
 /*------------------открытие и закрытие меню------------------*/
 if (icon_menu) {
@@ -42,34 +92,6 @@ if (menu_links.length > 0) {
       });
       e.preventDefault();
     }
-    // if (menuLink.dataset.goto === '#account_anchor'){
-    //   Open_close_menu();
-    // }
   }
 }
 /*------------------плавная прокрутка по якорям------------------*/
-
-
-
-
-
-
-// console.log(menuLink.dataset.goto);
-
-//     if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
-//       const gotoBlock = document.querySelector(menuLink.dataset.goto);
-//       const gotoBlockValue = gotoBlock.getBoundingClientRect().
-//     }
-
-
-// <!-- <img class="close_window_cross" src="./images/social_media/window-close-line.svg" alt="window-close-line"></img> -->
-  // const cross = document.querySelector('.close_window_cross');
-  // console.log(cross);
-
-  // if (cross) {
-  //   cross.addEventListener("click", function (e) {
-  //     var nav = document.getElementById('navigation');
-  //     nav.classList.remove("_active");
-  //   })
-// icon_menu.classList.toggle('_active');
-// cross.style="display: block";
