8803e5cab01114cfb64a8b6ae9078a9b280e2db5
Rodion <rodionserez@gmail.com>
2022-08-02 10:45:40 +0300

    feat: image slider

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,9 +1,15 @@
+let body = document.querySelector('body');
 const time = document.querySelector('.time');
 const currentDate = document.querySelector('.date');
 const WeekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 const greetingText = document.querySelector('.greeting');
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
 let UserName = document.querySelector('.name');
+
+const MAX = 20, MIN = 1; // диапазон рандомных чисел для смены фона 
+let bgNum;
 
 
 function getTimeOfDay() {
@@ -23,7 +29,34 @@ function showTime() {
 	setTimeout(showTime, 1000);
 }
 
+const getRandomBgNum = () => bgNum = Math.floor(Math.random() * (Math.floor(MAX) - Math.ceil(MIN) + 1)) + Math.ceil(MIN);
+
+const setBg = () => {
+	let timeOfDay = getTimeOfDay();
+	bgNum < 10 ? bgNum = String(String(bgNum).padStart(2, '0')) : bgNum = String(bgNum);
+	const img = new Image();
+	img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+  img.onload = () => {
+    body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+  };
+}
+
+const getSlidePrev = () => {
+	bgNum = parseInt(bgNum, 10);
+	bgNum - 1 < 1 ? bgNum = MAX : bgNum -= 1;
+	setBg();
+}
+
+const getSlideNext = () => {
+	bgNum = parseInt(bgNum, 10);
+	bgNum + 1 > 20 ? bgNum = MIN : bgNum += 1;
+	setBg();
+}
+
+
 showTime();
+getRandomBgNum();
+setBg();
 
 
 const setLocalStorage = () => localStorage.setItem('name', UserName.value);
@@ -33,3 +66,6 @@ const getLocalStorage = () => { if(localStorage.getItem('name')) UserName.value 
 window.addEventListener('beforeunload', setLocalStorage) // сохранить имя пользователя в local storage перед перезагрузкой или закрытием страницы
 
 window.addEventListener('load', getLocalStorage) // загрузить имя пользователя из local storage при загрузке страницы
+
+slidePrev.addEventListener('click', getSlidePrev)
+slideNext.addEventListener('click', getSlideNext)
