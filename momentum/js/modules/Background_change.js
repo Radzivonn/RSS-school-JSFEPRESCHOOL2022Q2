66bd0579ee8296f2db934e3337eec23970528f4b
Rodion <rodionserez@gmail.com>
2022-08-13 16:17:33 +0300

    feat: getting BG image from API

--- momentum/js/modules/Background_change.js
+++ momentum/js/modules/Background_change.js
@@ -4,8 +4,35 @@ import {getRandomNum, MAX, MIN} from './General_module.js';
 const body = document.querySelector('body');
 const slideNext = document.querySelector('.slide-next');
 const slidePrev = document.querySelector('.slide-prev');
+let timeOfDay = getTimeOfDay(false);
 let bgNum;
 let pageLoaded = false;
+let url = ``;
+let imagesListFromFlickrAPI = [];
+let imagesListLen = 0;
+
+let BGphotoSource = 'GitHub';
+
+
+async function getLinkByUnsplashAPI() {
+  const res = await fetch(`https://api.unsplash.com/photos/random?query=${timeOfDay}&client_id=K_JQbbDUaSngxKk6lGvvkJaowbRwXZgCK98WCmjBPjM`);
+  const data = await res.json();
+  return data.urls.regular;
+}
+
+
+async function getLinkByFlickrAPI() {
+  const res = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=9890e6668e76db4fc54684a689e616f4&tags=${timeOfDay}&extras=url_l&format=json&nojsoncallback=1`);
+  let data = await res.json();
+  return data.photos.photo;
+}
+
+getLinkByFlickrAPI().then(data => {
+	imagesListFromFlickrAPI = data;// достаём список фото из ассинхронной функции
+	imagesListLen = imagesListFromFlickrAPI.length;
+	setBg();
+});
+
 
 export default function setBg() {;
 	if(!pageLoaded){
@@ -13,12 +40,19 @@ export default function setBg() {;
 		pageLoaded = true;
 	}
 	bgNum < 10 ? bgNum = String(String(bgNum).padStart(2, '0')) : bgNum = String(bgNum);
-	let timeOfDay = getTimeOfDay(false);
+	timeOfDay = getTimeOfDay(false);
+	getLinkBySource(BGphotoSource); // get Photo url by source
 	const img = new Image();
-	img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+	img.src = url;
   img.onload = () => {
-    body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+    body.style.backgroundImage = `url(${url})`;
   };
+}
+
+const getLinkBySource = (Source) => {
+	if (Source === 'GitHub') url = `https://raw.githubusercontent.com/Radzivonn/stage1-tasks-images/assets/images/${timeOfDay}/${bgNum}.jpg`;
+	else if (Source === 'Unsplash API') getLinkByUnsplashAPI().then(data => url = data); // достаём data из ассинхронной функции;
+	else if (Source === 'Flickr API') url = imagesListFromFlickrAPI[Math.floor(Math.random() * (Math.floor(imagesListLen) - Math.ceil(0) + 1)) + Math.ceil(0)].url_l;
 }
 
 const getSlidePrev = () => {
@@ -35,3 +69,5 @@ const getSlideNext = () => {
 
 slidePrev.addEventListener('click', getSlidePrev);
 slideNext.addEventListener('click', getSlideNext);
+
+if (BGphotoSource !== 'Flickr API') setBg();
--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,6 +1,5 @@
 import {getRandomNum, MAX, MIN} from './modules/General_module.js';
 import {showTime} from './modules/Date_and_greeting.js';
-import setBg from './modules/Background_change.js';
 import getWeather from './modules/weather_widget.js';
 
 let UserName = document.querySelector('.name');
@@ -8,10 +7,8 @@ let UserName = document.querySelector('.name');
 const setLocalStorage = () => localStorage.setItem('name', UserName.value);
 const getLocalStorage = () => { if(localStorage.getItem('name')) UserName.value = localStorage.getItem('name') }
 
-
 showTime();
 getRandomNum(MIN, MAX);
-setBg();
 getWeather();
 
 window.addEventListener('beforeunload', setLocalStorage); // сохранить имя пользователя в local storage перед перезагрузкой или закрытием страницы
