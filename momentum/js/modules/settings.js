e5d97de041ac2ccc1d87ab989babbe0cbc3d616a
Rodion <106487154+Radzivonn@users.noreply.github.com>
2022-08-28 17:24:51 +0300

    Delete momentum directory

--- momentum/js/modules/settings.js
+++
@@ -1,9 +0,0 @@
-const settingsState = {
-	"language": 'en',
-	"city": localStorage.getItem("city"),
-  "photoSource": 'GitHub',
-  "tagInput": 'disabled',
-  "tagForBG": 'nature',
-  "AllWidgets": ['time', 'date','greeting-container', 'quotes__block', 'weather', 'player', 'social-media'],
-  "IncludedWidgets": ['time', 'date','greeting-container', 'quotes__block', 'weather', 'player', 'social-media'],
-}
